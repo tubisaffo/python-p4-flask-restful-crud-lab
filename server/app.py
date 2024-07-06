775bc51e14ba7e810dbf79d94c7f71cdd1d2280b
tubisaffo <tubisaffo@gmail.com>
2024-07-07 01:50:24 +0300

    restful crud

--- server/app.py
+++ server/app.py
@@ -44,8 +44,51 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+
+        response_dict = plant.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+    
+    
+    def patch(self, id):
+
+        record = Plant.query.filter(Plant.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+    
+    def delete(self, id):
+
+        record = Plant.query.filter(Plant.id == id).first()
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "Record deleted"}
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+    
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
